ab22b5b4b972e9f781050f9cf1a919ad92c75592
Hu Yufeng <hyf2k@163.com>
2016-05-17 13:46:09 +0800

    merge the frame work

---
+++ src/HtmlPaser.py
@@ -0,0 +1,29 @@
+'''
+Created on 2016Äê5ÔÂ17ÈÕ
+
+@author: huyufeng
+'''
+
+
+class HtmlPaser(object):
+    
+    
+    def __init__(self):
+        pass
+
+    
+    def GetUrls(self, response):
+        pass
+
+    
+    def GetInfo(self, response):
+        pass
+    
+    
+    
+    
+    
+    
+
+
+
---
+++ src/UrlSpider.py
@@ -0,0 +1,23 @@
+'''
+Created on 2016Äê5ÔÂ17ÈÕ
+
+@author: huyufeng
+'''
+
+
+class UrlSpider(object):
+    
+    
+    def __init__(self):
+        pass
+
+    
+    def Claw(self, url):
+        pass
+    
+    
+    
+    
+
+
+
